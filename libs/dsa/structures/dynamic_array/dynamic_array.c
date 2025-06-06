a849c777d353cc73083da696ea5c2abfe95a51cc
AdrianRo147 <adrian.rovnan@gmail.com>
2025-06-07 00:36:32 +0200

    implemented dynamic array structure

--- libs/dsa/structures/dynamic_array/dynamic_array.c
+++ libs/dsa/structures/dynamic_array/dynamic_array.c
@@ -1,1 +1,119 @@
-#include "static_array.h"
+#include "dynamic_array.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Sets member variables of DynamicArray.
+ * @param array Pointer to DynamicArray. This should be already allocated structure.
+ * @param count Count of needed elements.
+ */
+void dynamic_array_init(DynamicArray *array, size_t count)
+{
+    array->count = count;
+    array->elements = (void**)calloc(count, sizeof(void*));
+}
+
+/**
+ * @brief Inserts new element to array.
+ * @param array Pointer to DynamicArray.
+ * @param element Element to insert.
+ * @return True if element was inserted, otherwise return false.
+ */
+bool dynamic_array_push(DynamicArray *array, void *element)
+{
+    array->count++;
+    void **new_array = (void**)malloc(array->count * sizeof(void*));
+
+    if (new_array == NULL)
+        return false;
+
+    for (size_t i = 0; i < array->count - 1; i++)
+    {
+        *(new_array + i) = *(array->elements + i);
+    }
+
+    *(new_array + array->count - 1) = element;
+
+    free(array->elements);
+    array->elements = NULL;
+
+    array->elements = new_array;
+
+    return true;
+}
+
+/**
+ * @brief Removes element by index.
+ * @param array Pointer to DynamicArray.
+ * @param index Index of element to remove.
+ * @return True if element removal was successful, otherwise return false.
+ */
+bool dynamic_array_pop(DynamicArray *array, size_t index)
+{
+    if (index >= array->count)
+        return false;
+
+    array->count--;
+
+    void **new_array = (void**)malloc(array->count * sizeof(void*));
+
+    for (size_t i = 0; i <= array->count; i++)
+    {
+        if (i == index)
+            continue;
+
+        if (i < index)
+            *(new_array + i) = *(array->elements + i);
+
+        if (i > index)
+            *(new_array + i - 1) = *(array->elements + i);
+    }
+
+    free(array->elements);
+    array->elements = NULL;
+
+    array->elements = new_array;
+
+    return true;
+}
+
+/**
+ * @brief Gets element at specific index.
+ * @param array Pointer to DynamicArray.
+ * @param index Index to set.
+ */
+void *dynamic_array_get(DynamicArray *array, size_t index)
+{
+    if (index >= array->count)
+        return NULL;
+
+    return *(array->elements + index);
+}
+
+/**
+ * @brief Sets element at specific index.
+ * @param array Pointer to DynamicArray.
+ * @param index Index to set.
+ * @param element Pointer to element to set.
+ */
+bool dynamic_array_set(DynamicArray *array, size_t index, void *element)
+{
+    if (index >= array->count)
+        return false;
+
+    *(array->elements + index) = element;
+
+    return true;
+}
+
+/**
+ * @brief Deallocated heap allocated member variables of DynamicArray.
+ * @param array Pointer to DynamicArray.
+ */
+void dynamic_array_destroy(DynamicArray *array)
+{
+    array->count = 0;
+    free(array->elements);
+    array->elements = NULL;
+}
--- libs/dsa/structures/dynamic_array/dynamic_array.h
+++ libs/dsa/structures/dynamic_array/dynamic_array.h
@@ -1,1 +1,58 @@
-#pragma once
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct _DynamicArray
+{
+    size_t count;
+    void **elements;
+} DynamicArray;
+
+/**
+ * @brief Sets member variables of DynamicArray.
+ * @param array Pointer to DynamicArray. This should be already allocated structure.
+ * @param count Count of needed elements.
+ */
+void dynamic_array_init(DynamicArray *array, size_t count);
+
+/**
+ * @brief Inserts new element to array.
+ * @param array Pointer to DynamicArray.
+ * @param element Element to insert.
+ * @return True if element was inserted, otherwise return false.
+ */
+bool dynamic_array_push(DynamicArray *array, void *element);
+
+/**
+ * @brief Removes element by index.
+ * @param array Pointer to DynamicArray.
+ * @param index Index of element to remove.
+ * @return True if element removal was successful, otherwise return false.
+ */
+bool dynamic_array_pop(DynamicArray *array, size_t index);
+
+/**
+ * @brief Gets element at specific index.
+ * @param array Pointer to DynamicArray.
+ * @param index Index to set.
+ */
+void *dynamic_array_get(DynamicArray *array, size_t index);
+
+/**
+ * @brief Sets element at specific index.
+ * @param array Pointer to DynamicArray.
+ * @param index Index to set.
+ * @param element Pointer to element to set.
+ */
+bool dynamic_array_set(DynamicArray *array, size_t index, void *element);
+
+
+/**
+ * @brief Deallocated heap allocated member variables of DynamicArray.
+ * @param array Pointer to DynamicArray.
+ */
+void dynamic_array_destroy(DynamicArray *array);
+
+#endif
